f46516c3a58158222f895ff554d4c44625b4c241
howcome-dev <monmari882@gmail.com>
2020-05-24 01:58:58 +0900

    いったんCSSで調整

--- assets/js/opening.js
+++ assets/js/opening.js
@@ -1,16 +1,16 @@
-// 要素を取得
-const element = document.querySelector('.home');
-element.animate(
-  {
-    transform: [
-      'translateY(0px)', // 開始値
-      'translateY(420px)' // 終了値
-    ]
-  },
-  {
-    duration: 2500, // ミリ秒指定
-    iterations: 1, // 繰り返し回数
-    direction: 'normal', // 繰り返し挙動
-    easing: 'cubic-bezier(0.83, 0, 0.17, 1)' // 加減速種類
-  }
-);
+// // 要素を取得
+// const element = document.querySelector('.home');
+// element.animate(
+//   {
+//     transform: [
+//       'translateY(0px)', // 開始値
+//       'translateY(420px)' // 終了値
+//     ]
+//   },
+//   {
+//     duration: 2500, // ミリ秒指定
+//     iterations: 1, // 繰り返し回数
+//     direction: 'normal', // 繰り返し挙動
+//     easing: 'cubic-bezier(0.83, 0, 0.17, 1)' // 加減速種類
+//   }
+// );
