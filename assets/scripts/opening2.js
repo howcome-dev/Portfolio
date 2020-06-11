c0d6e33c82a5d90b5909bf25e96b9c01ef7a5a73
howcome-dev <monmari882@gmail.com>
2020-06-11 17:16:26 +0900

    ？の動き修正

--- assets/scripts/opening2.js
+++ assets/scripts/opening2.js
@@ -2,20 +2,20 @@
 
 // ？が降ってくる
 let questionTransform = [
-  { transform: 'scale(0.1) translate3D(50%, 0, 0) rotate(-50deg)' }, // 右に傾きながらフレームの上から降る
-  { transform: 'translate3D(0, 20vh, 0) rotate(160deg)' }, // 右にかたむきながらHowcomeと一緒におちる
-  { transform: 'scale(5) translate3D(0, 10vh, 0) rotate(360deg)' }, // 大きくなって一回転しながら真ん中へ
-  { transform: 'translate3D(0, 20vh, 0) rotate(-360deg)' }, // 元の大きさに戻って逆回転
-  { transform: 'translate3D(40%, 25vh, 0) rotate(10deg)' }, // Howcome?の位置へ
-  { transform: 'translate3D(30%, 30vh, 0) rotate(-80deg)' }, // 左へぽんぽん進む
-  { transform: 'translate3D(20%, 35vh, 0) rotate(10deg)' },
-  { transform: 'translate3D(10%, 25vh, 0) rotate(10deg)' },
-  { transform: 'scale(2) translate3D(0, 15vh, 0) rotate(10deg)' },
-  { transform: 'scale(3) translate3D(-10%, 10vh, 0) rotate(185deg)' },
-  { transform: 'scale(0) translate3D(-40%, 20vh, 0) rotate(-10deg)' }
+  { transform: 'scale(0) translate3D(50%, 0, 0) rotate(-50deg)' },
+  { transform: 'scale(3) translate3D(0, 1vh, 0) rotate(310deg)' }, // 大きくなって一回転しながら真ん中へ
+  { transform: 'scale(5) translate3D(0, 3vh, 0) rotate(390deg)' }, // 真ん中でちょっと止まる
+  { transform: 'scale(5) translate3D(0, 3vh, 0) rotate(400deg)' }, // 真ん中でちょっと止まる
+  { transform: 'translate3D(100%, 6vh, 0) rotate(20deg)' }, // ぴょんぴょんする
+  { transform: 'translate3D(300%, -5vh, 0) rotate(40deg)' },
+  { transform: 'translate3D(-30%, 5vh, 0) rotate(-10deg)' },
+  { transform: 'translate3D(-330%, -3vh, 0) rotate(-20deg)' },
+  { transform: 'scale(2) translate3D(0, 0, 0) rotate(35deg)' },
+  { transform: 'scale(25) translate3D(0, 3vh, 0) rotate(23deg)' },
+  { transform: 'scale(0) translate3D(0, 0, 0) rotate(-10deg)' }
 ];
 let questionTiming = {
-  duration: 6000,
+  duration: 3200,
   iterations: 1,
   fill: 'forwards',
   easing: 'ease-in'
